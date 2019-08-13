a535ba3c06628c82fdd5fc12f5fa3a58c94ce406
Alex Travagin Chatalov <alex_tr1992@hotmail.com>
2019-08-13 20:01:21 -0300

    adicionado o returno para o frontend

--- src/main/java/com/orcamento/request/RendaController.java
+++ src/main/java/com/orcamento/request/RendaController.java
@@ -2,12 +2,16 @@ package com.orcamento.request;
 
 import com.orcamento.domain.Renda;
 import com.orcamento.service.RendaService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +23,9 @@ public class RendaController {
     @GetMapping(value = "/rendas")
     public @ResponseBody List<Renda> getRendas() {
         System.out.println(service.getRendas().size());
-        return service.getRendas();
+        List<Renda> rendas = new ArrayList<>();
+        rendas.add( Renda.builder().id("1").renda("teste").valor(BigDecimal.ONE).build());
+        rendas.add( Renda.builder().id("2").renda("teste2").valor(BigDecimal.ONE).build());
+        return rendas;
     }
 }
